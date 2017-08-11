1583e81124342df43e4fb6aeeb5f52494cca6273
Balazs Nemeth <balazs.nemeth@tmit.bme.hu>
2017-08-11 10:53:41 +0200

    Add new function to Toolbox to retrieve technology specific interdomain tag info

--- nffg.py
+++ nffg.py
@@ -2955,6 +2955,58 @@ class NFFGToolBox(object):
     return nffg
 
   @staticmethod
+  def retrieve_and_purge_all_tag_info (nffg):
+    """
+    Searches all match fields of all flowrules for some possible tag values,
+    which may come from inter domain traffic steering and may use technology
+    specific tagging for a neighbouring domain.
+    This info is gathered for all incoming flowrule sequences and returned in
+    a dictionary keyed by the flowrule ID/SGHop ID. The gathered tags are
+    deleted from all encountered flowrules (thus the input NFFG is modified).
+    :param nffg:
+    :return: dict indexed by flowrule ID.
+    """
+    # the set of tags which shall be considered. Possibly needed to modify!
+    possible_tag_infos = ("dl_vlan", "mpls_label")
+    # WARNING: we can't differentiate ethertypes based only on the presence of
+    # mpls_label, other fields should be processed too. But only one layer of
+    # MLPS is supported in OVS currently, whose format we use in Flowrules
+    untag_actions = ("strip_vlan", "pop_mpls:0x8847")
+    tag_info_all_sghops = {}
+    for infra in nffg.infras:
+      for fr in infra.flowrules():
+        for match_element in fr.match.split(";"):
+          match_element_splitted = match_element.split("=")
+          if len(match_element_splitted) == 2:
+            if match_element_splitted[0] in possible_tag_infos:
+              if fr.id not in tag_info_all_sghops:
+                # save the tag_info
+                tag_info_all_sghops[fr.id] = match_element
+              elif tag_info_all_sghops[fr.id] != match_element:
+                # we have found another flowrule which has a different
+                # possible_tag_info.
+                raise RuntimeError(
+                  "The flowrule sequence of flowrule %s in infra %s has "
+                  "multiple match fields "
+                  "which may be used for interdomain traffic steering ("
+                  "%s) so it cannot be decided which one to use." %
+                  (fr, infra.id, possible_tag_infos))
+              # delete this match element from the match of the flowrule
+              # and the ; separators too, in case they are left on the
+              # beginning or ending.
+              # we can delete this tag info from other flowrules of the same
+              # flowrule sequence too, because the abstract tag will be used
+              # during the mapping.
+              fr.match = fr.match.replace(tag_info_all_sghops[fr.id], ""). \
+                                  rstrip(";").lstrip(";")
+    # we need to add the corresponding untag actions for every tag_info field.
+    for sgid in tag_info_all_sghops:
+      for tag_info, untag_action in zip(possible_tag_infos, untag_actions):
+        if tag_info in tag_info_all_sghops[sgid]:
+          tag_info_all_sghops[sgid] = (tag_info_all_sghops[sgid], untag_action)
+    return tag_info_all_sghops
+
+  @staticmethod
   def redirect_flowrules (from_port, to_port, infra, mark_external=False,
                           log=logging.getLogger("MOVE")):
     """
